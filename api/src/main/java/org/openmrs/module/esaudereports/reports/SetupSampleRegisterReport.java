6a1904624e6651bb72261e1e015946c11b369460
ningosi <nicotwendelee@gmail.com>
2017-07-19 16:43:45 +0300

    Added sample report

---
+++ api/src/main/java/org/openmrs/module/esaudereports/reports/SetupSampleRegisterReport.java
@@ -0,0 +1,87 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.esaudereports.reports;
+
+import org.openmrs.module.esaudereports.EsaudeDataExportManager;
+import org.openmrs.module.reporting.data.person.definition.PreferredNameDataDefinition;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+
+/**
+ * Created by Nicholas Ingosi on 7/19/17. A smaple register listing vaious patient information
+ */
+public class SetupSampleRegisterReport extends EsaudeDataExportManager {
+	
+	/**
+	 * @return the uuid for the report design for exporting to Excel
+	 */
+	@Override
+	public String getExcelDesignUuid() {
+		return "cc0fa186-6c83-11e7-9fd6-507b9dc4c741";
+	}
+	
+	/**
+	 * Build the report design for the specified report, this allows a user to override the report
+	 * design by adding properties and other metadata to the report design
+	 * 
+	 * @param reportDefinition
+	 * @return The report design
+	 */
+	@Override
+	public ReportDesign buildReportDesign(ReportDefinition reportDefinition) {
+		return null;
+	}
+	
+	@Override
+	public String getUuid() {
+		return "bf60e44a-6c83-11e7-9a26-507b9dc4c741";
+	}
+	
+	@Override
+	public String getName() {
+		return "Sample Report";
+	}
+	
+	@Override
+	public String getDescription() {
+		return "Contain sample information for the patient";
+	}
+	
+	@Override
+	public ReportDefinition constructReportDefinition() {
+		ReportDefinition rd = new ReportDefinition();
+		
+		rd.setUuid(getUuid());
+		rd.setName(getName());
+		rd.setDescription(getDescription());
+		rd.addParameters(getParameters());
+		rd.addDataSetDefinition("Sample", Mapped.mapStraightThrough(dataSetDefinition()));
+		return rd;
+	}
+	
+	@Override
+	public String getVersion() {
+		return "0.1";
+	}
+	
+	private DataSetDefinition dataSetDefinition() {
+		PatientDataSetDefinition dsd = new PatientDataSetDefinition();
+		dsd.addColumn("Patient Names", new PreferredNameDataDefinition(), (String) null);
+		//add more colums here as required
+		return dsd;
+	}
+}
